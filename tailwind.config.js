422611af35c5643579555a73896987e6a4798b75
apple <apple@apples-MacBook-Pro.local>
2024-04-27 14:04:13 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["*"];
+export const theme = {
+  screens: {
+    'xs': '450px'
+  },
+  extend: {},
+};
+export const plugins = [];
+
